1c4bd25d63091cf6e69e54784c0ab99d3526d5f9
Norman Teodoro <normanteodoro@hotmail.com>
2022-04-08 16:28:23 -0700

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Web Developer / Lover{' '}
+          Web Developer{' '}
           {/* <code className={styles.code}>pages/index.js</code> */}
         </p>
 
